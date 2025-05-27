7052afd24ee84aa2fd9d6aeb76e594d97dc2f596
GilpiDv <gilgonzalez981@gmail.com>
2025-05-26 22:16:41 -0600

    Create functions for update and delete in the zustand store

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,11 +1,15 @@
 import type { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store"
 
 type PatientDetailsProps = {
     patient: Patient
 }
 
 export default function PatientDetails({patient} : PatientDetailsProps) {
+
+    const { deletePatient, getPatientById } = usePatientStore();
+
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label={'ID'} data={patient.id} />
@@ -15,16 +19,18 @@ export default function PatientDetails({patient} : PatientDetailsProps) {
             <PatientDetailItem label={'Date'} data={patient.date.toString()} />
             <PatientDetailItem label={'Symptoms'} data={patient.symptoms} />
 
-            <div className="flex justify-between mt-10">
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
                 <button 
                     className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg cursor-pointer"
                     type="button"
+                    onClick={() => getPatientById(patient.id)}
                 >
                     Edit
                 </button>
                 <button 
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg cursor-pointer"
                     type="button"
+                    onClick={() => deletePatient(patient.id)}
                 >
                     Delete
                 </button>
